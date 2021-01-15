aff852d523129fa0033325226d43de46955f16bb
Dheerajzxx <76279411+Dheerajzxx@users.noreply.github.com>
2021-01-15 23:02:45 +0530

    Update users.php

--- users.php
+++ users.php
@@ -9,8 +9,9 @@ if($idkey!='admin1'){
 
 $search = $_GET['search'] ?? '';
 if($search){
-  $statement = $pdo->prepare('SELECT*FROM users WHERE firstname LIKE :search OR surname LIKE :search OR phone LIKE :search OR email LIKE :search OR password LIKE :search ORDER BY firstname ASC');  
+  $statement = $pdo->prepare('SELECT*FROM users WHERE idkey LIKE :idkey AND firstname LIKE :search OR surname LIKE :search OR phone LIKE :search OR email LIKE :search OR password LIKE :search ORDER BY firstname ASC');  
   $statement->bindValue(':search', "%$search%");
+  $statement->bindValue(':idkey', $idkey);
 }
 else{
 $statement = $pdo->prepare('SELECT*FROM users ORDER BY firstname ASC');
